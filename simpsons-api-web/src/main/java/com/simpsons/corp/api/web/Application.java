626fec1564a0f24b3b83758ab7e31a059a213cce
Sergiu, Dobrovolschi <sergiu.dobrovolschi@gmail.com>
2019-06-18 22:49:14 +0100

    Defined Spring Boot app on port 8083

---
+++ simpsons-api-web/src/main/java/com/simpsons/corp/api/web/Application.java
@@ -0,0 +1,28 @@
+package com.simpsons.corp.api.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.simpsons.corp.api.web",
+        "com.simpsons.corp.api.model",
+        "com.simpsons.corp.api.service"
+})
+@EnableMongoRepositories(basePackages = "com.simpsons.corp.api.repositories")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
